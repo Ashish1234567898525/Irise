30c9bbfd50ef927925a6ee1675a19e61a98d0b2e
Ashish1234567898525 <144302300+Ashish1234567898525@users.noreply.github.com>
2023-10-05 14:32:07 +0530

    updatews

---
+++ Day-3/Login form/Resume/CSS/border/Javascript/Day-2-js.js/Day-2-js.js
@@ -0,0 +1,15 @@
+var num = 20;
+if ( 20<10){
+    console.log("20 is greater than 10")
+}else{
+    console.log("10 is less than 20")
+}
+
+var time =8;
+if(time>12 && time<24){
+    console.log("Thankyou")
+}else if(time>8 && time<16){
+    console.log("Welcome")
+}else if(time>15 && time <28)
+   { console.log("Namaste")
+}
