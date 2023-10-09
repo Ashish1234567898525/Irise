20d009aa6037286cfb17d56f4ccc544db7efbf88
Ashish1234567898525 <144302300+Ashish1234567898525@users.noreply.github.com>
2023-10-09 16:14:08 +0530

    updates

---
+++ Day-3/Login form/Resume/CSS/border/Javascript/Day-4-js.js/Day-4-js.js
@@ -0,0 +1,14 @@
+var number =5;
+number++;
+number = number+5;
+number++;
+number++;
+number--;
+number = number -5;
+console.log(number)
+
+
+for (var l=5; l >= 300; l++ ){
+
+    console.log(l)
+}
